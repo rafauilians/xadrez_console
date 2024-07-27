007496252cd1087a720e740b9adfbe55da3c5b71
rafauilians <rafa.uilians@hotmail.com>
2024-07-27 15:22:04 -0300

    Movimentos do Rei

--- xadrez_console/tabuleiro/Peca.cs
+++ xadrez_console/tabuleiro/Peca.cs
@@ -1,17 +1,17 @@
 ﻿namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
-        public int qteMovimentos { get; protected set;}
+        public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
         public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = null; 
-            this.tab = tab; 
-            this.cor = cor; 
+            this.posicao = null;
+            this.tab = tab;
+            this.cor = cor;
             this.qteMovimentos = 0;
         }
 
@@ -19,5 +19,7 @@
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
