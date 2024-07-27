3a2438170a93c5472cdca3317d354cfe347d6cd6
rafauilians <rafa.uilians@hotmail.com>
2024-07-27 14:14:42 -0300

    Criando posição Xadrez

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -8,13 +8,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-            Tela.imprimirTabuleiro(tab);
+            Console.WriteLine(pos);
 
             Console.ReadLine();
         }
